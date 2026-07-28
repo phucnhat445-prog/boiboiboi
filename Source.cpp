e13a0a29e2ef7840abf4d7c76471679789f8657f
trangiabao <giabaoooqwq@gmail.com>
2026-07-28 11:38:46 +0700

    them vao main mot xiu

--- Source.cpp
+++ Source.cpp
@@ -1,29 +1,44 @@
+#include <iostream>
 #include <string>
 #include "Figure.h"
 #include "Rectangle.h"
-#include "Point.h"
 #include "Triangle.h"
+#include "Square.h"   
+#include "Ellipse.h"  
+#include "Circle.h"   
 
 using namespace std;
-int main() {
 
+int main() {
     Figure* arr[] = {
-        new Rectangle(),  new Triangle()
+        new Rectangle(),
+        new Triangle(),
+        new Square(),
+        new Circle(),
+        new Ellipse()
     };
     int narr = sizeof(arr) / sizeof(Figure*);
 
+    cout << "Cac hinh co san:";
+    for (int i = 0; i < narr; i++) {
+        cout << " " << arr[i]->className();
+    }
+    cout << endl;
+
     string inputName;
-    cout << "Nhap ten hinh (Rectangle, Triangle): ";
+    cout << "Nhap ten hinh ban muon tao: ";
     cin >> inputName;
 
     Figure* rec = Figure::createObject(inputName.c_str());
 
     if (rec != nullptr) {
+
+        cout << "User's choice: " << rec->className() << endl;
+
         cout << "\n--- Nhap du lieu ---" << endl;
         rec->Input(cin);
 
         cout << "\n--- Ket qua ---" << endl;
-        
         rec->Output(cout);
         cout << endl;
 
